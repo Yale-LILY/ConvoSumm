2f05a14ae29336c0ef71fc33346f8bf49b5400c1
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 16:59:04 +0000

    Adding numeric_owner as keyword arguement

--- code/longformer-code/tests/test_readme.py
+++ code/longformer-code/tests/test_readme.py
@@ -42,7 +42,7 @@ class TestReadme(unittest.TestCase):
               if not is_within_directory(path, member_path):
                   raise Exception("Attempted Path Traversal in Tar File")
       
-          tar.extractall(path, members, numeric_owner) 
+          tar.extractall(path, members, numeric_owner=numeric_owner) 
           
       
       safe_extract(tar, "/tmp")
